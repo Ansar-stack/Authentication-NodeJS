c9c222a4a706366b7ac745e656dc67f346c965bd
Ansar-stack <ansar.stack@gmail.com>
2026-01-15 22:27:38 +0430

    removed res.status().json() and added res.respond()

--- src/middlewares/jwtAuth.middleware.js
+++ src/middlewares/jwtAuth.middleware.js
@@ -1,13 +1,11 @@
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 import { generateAccessToken } from "../utils/genToken.util.js";
-
+// jwt authentication middleware
 export const jwtAuth = async (req, res, next) => {
   const accessToken = req.headers.authorization?.split(" ")[1];
   if (!accessToken)
-    return res
-      .status(401)
-      .json({ success: false, message: "No access token provided" });
+    return res.respond(401,"No access token provided");
   //  Verfiy the access token
   try {
     const decode = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
@@ -15,29 +13,25 @@ export const jwtAuth = async (req, res, next) => {
     next();
   } catch (error) {
     if (error.name !== "TokenExpiredError") {
-      return res.status(401).json({ success: false, message: "Invalid Token" });
+      return res.respond(401, "Invalid Token");
     }
 
     //  Try To Refresh Token
     const refreshToken = req.cookies.refreshToken;
     if (!refreshToken)
-      return res.status(401).json({ success: false, message: "Unauthorized" });
+      return res.respond(401, "Unauthorized");
     //   Find token in DB
     const userFound = await User.findOne({ refreshToken });
     if (!userFound)
-      return res.status(401).json({ success: false, message: "Unauthorized" });
+      return res.respond(401, "Unauthorized");
     //  Validate the refresh token
     const payload = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECERET);
     if (
       !userFound.id === payload.userId ||
       !userFound.refreshToken === refreshToken
     )
-      return res.status(401).json({
-        success: false,
-        message: "Invalid Refresh Token",
-      });
+      return res.respond(401, "Unauthorized");
     const accessToken = generateAccessToken(userFound.id);
-    console.log(accessToken)
     next();
   }
 };
